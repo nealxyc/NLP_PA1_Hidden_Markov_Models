a020f5da0ab36752aac039d5f8db8a7ddbadf150
Neal Xiong <nealxyc@gmail.com>
2014-10-29 22:41:39 -0700

    Add maven support

--- test/utils/LinesIteratorTest.java
+++ test/utils/LinesIteratorTest.java
@@ -1,15 +1,12 @@
 package utils;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 import java.io.StringBufferInputStream;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-import org.hamcrest.core.AnyOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -38,15 +35,8 @@ public class LinesIteratorTest {
 	public void testIterable(){
 		LinesIterator itr = new LinesIterator(new StringBufferInputStream("abc\ndef\n123"));
 		for(String s: itr){
-			assertThat(s, AnyOf<T>());
-			assertEquals("abc", itr.next());
-			assertTrue(itr.hasNext());
-			assertEquals("def", itr.next());
-			assertTrue(itr.hasNext());
-			assertEquals("123", itr.next());
+			assertThat(s, anyOf(equalTo("abc"), equalTo("def"), equalTo("123")));
 		}
-		
-		assertFalse(itr.hasNext());
 		
 		exception.expect(NoSuchElementException.class);
 		itr.next();
